26f110617fd01ac20bb830c769fec8f1ccac48f6
L.Tonelli <lucietonelli@gmail.com>
2024-04-30 16:51:59 +0200

    Ajoutez des fichiers projet.

---
+++ PowerRangeurWEB/Entities/LevelRangeur.cs
@@ -0,0 +1,9 @@
+﻿namespace PowerRangeurWEB.Entities
+{
+    public partial class LevelRangeur
+    {
+        public int IdLevelRangeur { get; set; } = 1;
+        public string NomLevelRangeur { get; set; } = "Apprenti";
+        public ICollection<User>? Users { get; set; }
+    }
+}
---
+++ PowerRangeurWEB/Entities/Mission.cs
@@ -0,0 +1,14 @@
+﻿namespace PowerRangeurWEB.Entities
+{
+    public partial class Mission
+    {
+        public int IdMission { get; set; }
+        public required string NomMission { get; set; }
+        public required string DescriptionMission { get; set; }
+        public DateTime? MissionDate { get; set; }
+        public int BonusMission { get; set; }
+        public ICollection<User>? Users { get; set; }
+
+        public BonusMalus? BonusMalus { get; set; }
+    }
+}
