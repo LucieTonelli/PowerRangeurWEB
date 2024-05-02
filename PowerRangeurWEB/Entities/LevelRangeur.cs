a9158f9b55b3b278d026be896544d7ec5bc74484
L.Tonelli <lucietonelli@gmail.com>
2024-05-02 16:26:51 +0200

    début affichage tache

--- PowerRangeurWEB/Entities/LevelRangeur.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PowerRangeurWEB.Entities
-{
-    public partial class LevelRangeur
-    {
-        public int IdLevelRangeur { get; set; } = 1;
-        public string NomLevelRangeur { get; set; } = "Apprenti";
-        public ICollection<User>? Users { get; set; }
-    }
-}
---
+++ PowerRangeurWEB/Pages/TachePage.razor.cs
@@ -0,0 +1,29 @@
+﻿namespace PowerRangeurWEB.Pages
+{
+    public partial class TachePage
+    {
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            //Afficher la liste des taches à faire via API
+        }
+
+        public void Abandonner(int IdTache)
+        {
+
+        }
+
+
+        public void Prendre(int IdTache)
+        {
+
+        }
+
+        //public void GetAll
+        //{
+
+        //}
+
+    }
+}
