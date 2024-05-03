6f6ac8523d16ba09ec6dca325ec46cfcd9d9ce70
L.Tonelli <lucietonelli@gmail.com>
2024-05-03 15:09:53 +0200

    AddTache OK

--- PowerRangeurWEB/Pages/CreationTache.razor.cs
+++ PowerRangeurWEB/Pages/CreationTache.razor.cs
@@ -8,33 +8,66 @@ using System.Net.Http.Formatting;
 
 namespace PowerRangeurWEB.Pages
 {
+    //public partial class CreationTache
+    //{
+    //    private async Task CreerTache(TacheFormCreate tache)
+    //    {
+
+    //        Tache nouvelletache = new Tache()
+    //        {
+    //            NomTache = tache.NomTache,
+    //            Statut = tache.Statut,
+    //            PrioriteTaches = tache.PrioriteTaches,
+    //            Recurrence = tache.Recurrence,
+    //            Description = tache.Description,
+    //            DateEcheance = tache.DateEcheance,
+    //            PointBonusMalus = tache.PointBonusMalus
+    //        };
+    //        using var response = await HttpClient.PostAsJsonAsync<Tache>("api/tache", nouvelletache);
+    //        Article article = await response.Content.ReadFromJsonAsync<Article>();
+
+    //        HttpResponseMessage response = await HttpClient.PostAsJsonAsync("/api/tache", tache);
+    //        if (response.IsSuccessStatusCode)
+    //        {
+    //            await OnInitializedAsync();
+    //        }
+    //        else
+    //        {
+    //            await OnInitializedAsync();
+    //        }
+    //    }
+    //}
+
     public partial class CreationTache
     {
-        private async Task CreerTache(TacheFormCreate tache)
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public TacheFormCreate Tache { get; set; } = new TacheFormCreate();
+        private async Task CreerTache()
         {
 
-            Tache nouvelletache = new Tache()
-            {
-                NomTache = tache.NomTache,
-                Statut = tache.Statut,
-                PrioriteTaches = tache.PrioriteTaches,
-                Recurrence = tache.Recurrence,
-                Description = tache.Description,
-                DateEcheance = tache.DateEcheance,
-                PointBonusMalus = tache.PointBonusMalus
-            };
-            //using var response = await HttpClient.PostAsJsonAsync<Tache>("api/tache", nouvelletache);
-            //Article article = await response.Content.ReadFromJsonAsync<Article>();
 
-            //HttpResponseMessage response = await HttpClient.PostAsJsonAsync("/api/tache", tache);
-            //if (response.IsSuccessStatusCode)
+            //Tache nouvelletache = new Tache()
             //{
-            //    await OnInitializedAsync();
-            //}
-            //else
-            //{
-            //    await OnInitializedAsync();
-            //}
+            //    NomTache = tache.NomTache,
+            //    Statut = tache.Statut,
+            //    PrioriteTaches = tache.PrioriteTaches,
+            //    Recurrence = tache.Recurrence,
+            //    Description = tache.Description,
+            //    DateEcheance = tache.DateEcheance,
+            //    PointBonusMalus = tache.PointBonusMalus
+            //};
+
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync("/api/tache", Tache);
+            if (response.IsSuccessStatusCode)
+            {
+               Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine("Notok");
+            }
         }
     }
 }
---
+++ PowerRangeurWEB/Pages/DetailTache.razor.cs
@@ -0,0 +1,27 @@
+﻿using DocuSign.eSign.Model;
+using Microsoft.AspNetCore.Components;
+using PowerRangeurAPI.API.DTOs.Tache;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace PowerRangeurWEB.Pages
+{
+    public partial class DetailTache
+    {
+        private string? errorMessage;
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public TacheGet Tache { get; set; } = new TacheGet();
+
+        private async Task DetailTache()
+        {
+            using var httpResponse = await HttpClient.GetAsync("/api/tache");
+            if (!httpResponse.IsSuccessStatusCode)
+            { errorMessage = httpResponse.ReasonPhrase; return; }
+            UsersResponse response = await httpResponse.Content.ReadFromJsonAsync<UsersResponse>();
+
+        }
+    }
+}
