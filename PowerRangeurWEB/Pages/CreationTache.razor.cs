dc08b2fd44713269590ca5d9eab6b8bbe355f9b4
L.Tonelli <lucietonelli@gmail.com>
2024-05-03 13:50:50 +0200

    creation page

---
+++ PowerRangeurWEB/Pages/CreationTache.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using PowerRangeurAPI.API.DTOs.Tache;
+using PowerRangeurAPI.Domain.Models;
+using System.Net.Http.Json;
+using ZendeskApi_v2.Models.Articles;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+
+namespace PowerRangeurWEB.Pages
+{
+    public partial class CreationTache
+    {
+        private async Task CreerTache(TacheFormCreate tache)
+        {
+
+            Tache nouvelletache = new Tache()
+            {
+                NomTache = tache.NomTache,
+                Statut = tache.Statut,
+                PrioriteTaches = tache.PrioriteTaches,
+                Recurrence = tache.Recurrence,
+                Description = tache.Description,
+                DateEcheance = tache.DateEcheance,
+                PointBonusMalus = tache.PointBonusMalus
+            };
+            //using var response = await HttpClient.PostAsJsonAsync<Tache>("api/tache", nouvelletache);
+            //Article article = await response.Content.ReadFromJsonAsync<Article>();
+
+            //HttpResponseMessage response = await HttpClient.PostAsJsonAsync("/api/tache", tache);
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    await OnInitializedAsync();
+            //}
+            //else
+            //{
+            //    await OnInitializedAsync();
+            //}
+        }
+    }
+}
--- PowerRangeurWEB/Pages/TachePage.razor.cs
+++ PowerRangeurWEB/Pages/TachePage.razor.cs
@@ -1,29 +1,62 @@
-﻿namespace PowerRangeurWEB.Pages
+﻿using Microsoft.AspNetCore.Components;
+using PowerRangeurAPI.API.DTOs.Tache;
+using PowerRangeurAPI.Domain.Models;
+using System.Net.Http.Json;
+
+namespace PowerRangeurWEB.Pages
 {
     public partial class TachePage
     {
 
-        protected override void OnInitialized()
+        public IEnumerable<TacheGet> TachesLibres { get; set; } = [];
+        public IEnumerable<TacheGet> TachesEnCours { get; set; } = [];
+        public IEnumerable<TacheGet> TachesTerminees { get; set; } = [];
+
+
+
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        protected override async Task OnInitializedAsync()
         {
-            base.OnInitialized();
-            //Afficher la liste des taches à faire via API
+            List<TacheGet> taches = await HttpClient.GetFromJsonAsync<List<TacheGet>>("/api/tache/all");
+            TachesLibres = taches.Where(t => t.Statut == Tache.Status.Libre);
+            TachesEnCours = taches.Where(t => t.Statut == Tache.Status.Encours);
+            TachesTerminees = taches.Where(t => t.Statut == Tache.Status.Terminee);
         }
 
-        public void Abandonner(int IdTache)
+
+
+
+        private async Task CreerTache(TacheFormCreate tache)
         {
+            Tache nouvelletache = new Tache
+            {
+                NomTache = tache.NomTache,
+                Statut = tache.Statut,
+                PrioriteTaches = tache.PrioriteTaches,
+                Recurrence = tache.Recurrence,
+                Description = tache.Description,
+                DateEcheance = tache.DateEcheance,
+                PointBonusMalus = tache.PointBonusMalus
+            };
 
+            HttpResponseMessage response = await HttpClient.PostAsJsonAsync("/api/tache", tache);
+            if (response.IsSuccessStatusCode)
+            {
+                await OnInitializedAsync();
+            }
+            else
+            {
+                await OnInitializedAsync();
+            }
         }
+    
 
 
-        public void Prendre(int IdTache)
-        {
 
-        }
 
-        //public void GetAll
-        //{
 
-        //}
-
-    }
+}
 }
